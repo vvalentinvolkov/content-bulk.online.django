4e3ec4480c774b9285734b2cc15f4651443a1feb
bulkinegra <v.valentinvolkovwr@gmail.com>
2022-02-03 12:46:31 +0300

    static

--- app/app/dev_settings.py
+++ app/app/dev_settings.py
@@ -2,13 +2,19 @@ from .settings import *
 
 DEBUG = True
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001']
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sqlite3',
     }
 }
--- app/app/settings.py
+++ app/app/settings.py
@@ -93,4 +93,5 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = "/home/www/content-bulk.online/static/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
